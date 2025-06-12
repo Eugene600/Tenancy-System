d72d3ef06b8a91b73ed44526e9964a1580e75354
Eugene600 <ugowach160@gmail.com>
2025-06-12 12:17:30 +0300

    feat: Added a home component displaying home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faInstagram, faLinkedin, faXTwitter, faDiscord, faAmazon, faApple, faFacebook, faYoutube, faTiktok, faSnapchat, faPinterest, faReddit, faMedium, faTelegram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  faGithub = faGithub;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
+  faX = faXTwitter;
+  faDiscord = faDiscord;
+  faAmazon = faAmazon;
+  faApple = faApple;
+  faFacebook = faFacebook;
+  faYoutube = faYoutube;
+  faTiktok = faTiktok;
+  faSnapchat = faSnapchat;
+  faPinterest = faPinterest;
+  faReddit = faReddit;
+  faMedium = faMedium;
+  faTelegram = faTelegram;
+  faWhatsapp = faWhatsapp;
 }
